c20fc26213b44c48a8baac103062fc4047ab10b2
Python The Hack <lrulvoni@gmail.com>
2022-09-03 10:09:27 +0200

    update material

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, 
+DoCheck, OnDestroy  {
 
-  constructor() { }
+  constructor() {
+    console.log("costruttore");
+   }
+  ngAfterContentChecked(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngAfterContentInit(): void {
+      throw new Error('metodo errato');
+  }
+
+  ngDoCheck(): void {
+    console.log("costruttore"); 
+  }
+
+  ngOnDestroy(): void {
+      
+    console.log("costruttore");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("costruttore");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("costruttore");
+  }
+
 
   ngOnInit(): void {
+    console.log("ngOnInit");
   }
 
 }
